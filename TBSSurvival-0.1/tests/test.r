204d2f1812b0e190ffb1a39ef6d98e3f377893cc
cassiopc <cassiopc@gmail.com>
2012-06-22 18:40:31 +0200

    updated tests

--- TBSSurvival-0.1/tests/test.r
+++ TBSSurvival-0.1/tests/test.r
@@ -32,21 +32,30 @@ if(require("TBSSurvival",quietly=TRUE)==FALSE) {
 }
 options("warn" = w[[1]])
 
+####################
+## simple test with the GBSG2 (German Breast Cancer Group 2) data set from the ipred package
 library(ipred)
 data(GBSG2)
 s=tbs.survreg.mle(Surv(GBSG2$time,GBSG2$cens==1) ~ 1,dist="norm",method="Rsolnp",verbose=TRUE)
 s=tbs.survreg.mle(Surv(GBSG2$time,GBSG2$cens==1) ~ 1,dist="norm",method="BFGS",verbose=TRUE)
-s=tbs.survreg.mle(Surv(GBSG2$time,GBSG2$cens==1) ~ 1,dist="norm",method="Nelder-Mead",verbose=TRUE)
 
+
+####################
+## test with the colon data set from the survival package
 library(survival)
 data(colon)
 s=tbs.survreg.mle(Surv(colon$time,colon$status==1) ~ 1,dist="norm",method="Rsolnp",verbose=TRUE)
 s=tbs.survreg.mle(Surv(colon$time,colon$status==1) ~ 1,dist="norm",method="BFGS",verbose=TRUE)
 s=tbs.survreg.mle(Surv(colon$time,colon$status==1) ~ 1,dist="norm",method="Nelder-Mead",verbose=TRUE)
+
+## with covariate
 colon$age60=as.numeric(colon$age>60) #from medical paper
 s=tbs.survreg.mle(Surv(colon$time,colon$status==1) ~ colon$age60,dist="norm",method="Rsolnp",verbose=TRUE)
 s=tbs.survreg.mle(Surv(colon$time,colon$status==1) ~ colon$age60,dist="norm",method="BFGS",verbose=TRUE)
 s=tbs.survreg.mle(Surv(colon$time,colon$status==1) ~ colon$age60,dist="norm",method="Nelder-Mead",verbose=TRUE)
+
+
+
 
 
 ######################
@@ -54,8 +63,8 @@ s=tbs.survreg.mle(Surv(colon$time,colon$status==1) ~ colon$age60,dist="norm",met
 #             then compare the Weibull model with TBSmodel.
 #             Also, this function is used to analyse if TBSmodel MLE is 
 #             working well.
-simulation <- function(gen=list(n=100,cens=0,shape=2,scale=3),
-                       est=list(dist="norm"),n.copies=1,initial.seed=1234,method="Rsonlp") {
+sim.weib <- function(gen=list(n=100,cens=0,shape=2,scale=3),
+                     est=list(dist="norm"),n.copies=1,initial.seed=1234,method="Rsonlp") {
   # gen: is a list with the parameters to generate from a Weibull distribution
   #          n: sample size
   #       cens: censor rate
@@ -67,14 +76,14 @@ simulation <- function(gen=list(n=100,cens=0,shape=2,scale=3),
   # initial.seed: to control the simulation
 
   result.1  <- matrix(est$dist,n.copies,1)
-  result.2  <- matrix(NA,n.copies,20)
+  result.2  <- matrix(NA,n.copies,23)
 
   for (i in 1:n.copies) {
     # Generating data
     initial.time <- .gettime()
-    seed <- initial.seed+i-1
+    seed <- initial.seed+5*i-1
     set.seed(seed)
-    d <- list()
+    d <- NULL
     d$time  <- rweibull(gen$n,shape=1/gen$shape,scale=exp(gen$scale))
     d$delta <- rep(1,gen$n)
     censor  <- quantile(d$time,probs=(1-gen$cens))
@@ -103,17 +112,23 @@ simulation <- function(gen=list(n=100,cens=0,shape=2,scale=3),
                         max(abs(orig.y-weib.y)),mean(abs(orig.y-weib.y)),
                         ifelse(max(abs(orig.y-tbs.y)) > max(abs(orig.y-weib.y)), 0, 1),
                         ifelse(mean(abs(orig.y-tbs.y)) > mean(abs(orig.y-weib.y)), 0, 1),
-                        gen$scale*log(2)^(1/gen$shape),
+                        exp(gen$scale)*log(2)^gen$shape,median(d$time),
                         exp(tbs.fit$par[3]),
-                        exp(weib.fit$coefficients)*log(2)^weib.fit$scale,run.time)
+                        (exp(tbs.fit$par[3])-exp(gen$scale)*log(2)^gen$shape)^2,
+                        exp(weib.fit$coefficients)*log(2)^weib.fit$scale,
+                        (exp(weib.fit$coefficients)*log(2)^weib.fit$scale-
+                         exp(gen$scale)*log(2)^gen$shape)^2,run.time)
     } else {
       run.time <- .gettime() - initial.time
       result.2[i,] <- c(seed,gen$n,gen$cens,gen$shape,gen$scale,rep(NA,5),
                         1/weib.fit$scale,exp(weib.fit$coefficients),
                         max(abs(orig.y-weib.y)),mean(abs(orig.y-weib.y)),
-                        rep(NA,2),gen$scale*log(2)^(1/gen$shape),NA,
-                        exp(weib.fit$coefficients)*log(2)^weib.fit$scale,run.time)
+                        rep(NA,2),exp(gen$scale)*log(2)^gen$shape,median(d$time),NA,NA,
+                        exp(weib.fit$coefficients)*log(2)^weib.fit$scale,
+                        (exp(weib.fit$coefficients)*log(2)^weib.fit$scale-
+                         exp(gen$scale)*log(2)^gen$shape)^2,run.time)
     }
+    rm(d)
   }
   
   colnames(result.1) <- "dist"
@@ -121,13 +136,112 @@ simulation <- function(gen=list(n=100,cens=0,shape=2,scale=3),
                           "tbs.lambda","tbs.xi","tbs.beta","tbs.Max.AE",
                           "tbs.Mean.AE","weib.par1","weib.par2",
                           "weib.Max.AE","weib.Mean.AE","comp.Max.AE","comp.Mean.AE",
-                          "orig.median","tbs.median","weib.median","run.time")
+                          "orig.median","data.median","tbs.median","tbs.sqe",
+                          "weib.median","weib.sqe","run.time")
 
   return(result.2)
 }
 
 
-simulation(gen=list(n=1000,cens=0,scale=2,shape=3),est=list(dist="norm"),
-           n.copies=1,initial.seed=1235,method="BFGS")
+#aux <- sim.weib(gen=list(n=100,cens=0,scale=2,shape=3),est=list(dist="logistic"),
+#                n.copies=100,initial.seed=1235,method="BFGS")
+
+#apply(aux[,c(19,21)],2,mean); apply(aux[,c(19,21)],2,sd); apply(aux[,c(20,22)],2,mean)
 
 
+
+######################
+# test: function used to generate data from TBS model and 
+#       then check if the method is estimatin right!  
+sim.tbs <- function(gen=list(n=100,cens=0,lambda=2,xi=3,beta=1,dist="norm"),
+                    n.copies=1,initial.seed=1234,method="Rsonlp",verbose=FALSE) {
+  # gen: is a list with the parameters to generate from a TBS model
+  #          n: sample size
+  #       cens: censor rate
+  #     lambda, xi, beta: TBS parameters
+  # n.copies: how many copies will be performed in the simulation
+  # initial.seed: to control the simulation
+
+  result.1  <- matrix(gen$dist,n.copies,1)
+  result.2  <- matrix(NA,n.copies,19)
+
+  for (i in 1:n.copies) {
+    # Generating data
+    initial.time <- .gettime()
+    seed <- initial.seed+i-1
+    set.seed(seed)
+    d <- NULL
+    d$time  <- rtbs(gen$n,gen$lambda,gen$xi,gen$beta,dist=gen$dist)
+    d$delta <- rep(1,gen$n)
+    censor  <- quantile(d$time,probs=(1-gen$cens))
+    d$delta <- ifelse(d$time > censor, 0, 1)
+    d$time  <- ifelse(d$time > censor, censor, d$time)
+
+    # Original Survival function
+    orig.y  <- ptbs(d$time[d$delta == 1],gen$n,gen$lambda,gen$xi,gen$beta,dist=gen$dist)
+
+    # Tbs fit
+    tbs.fit  <- tbs.survreg.mle(Surv(d$time,d$delta) ~ 1,dist=gen$dist,method=method,verbose=verbose)
+
+    # Result matrix
+    if (tbs.fit$convergence) {
+      tbs.y <- ptbs(d$time[d$delta == 1],lambda=tbs.fit$par[1],xi=tbs.fit$par[2],
+                    beta=tbs.fit$par[3],dist=gen$dist)
+      run.time <- .gettime() - initial.time
+      result.2[i,] <- c(seed,gen$n,gen$cens,gen$lambda,gen$xi,gen$beta,tbs.fit$par,
+                        (gen$lambda-tbs.fit$par[1])^2,(gen$xi-tbs.fit$par[2])^2,
+                        (gen$beta-tbs.fit$par[3])^2,
+                        max(abs(orig.y-tbs.y)),mean(abs(orig.y-tbs.y)),median(d$time),
+                        exp(gen$beta),exp(tbs.fit$par[3]),(exp(tbs.fit$par[3])-exp(gen$beta))^2,
+                        run.time)
+    } else {
+      run.time <- .gettime() - initial.time
+      result.2[i,] <- c(seed,gen$n,gen$cens,gen$lambda,gen$xi,gen$beta,rep(NA,8),
+                        median(d$time),rep(NA,3),run.time)
+    }
+    rm(d)
+    print(i)
+  }
+  
+  colnames(result.1) <- "dist"
+  colnames(result.2) <- c("gen.seed","n","censor","orig.lambda","orig.xi","orig.beta",
+                          "tbs.lambda","tbs.xi","tbs.beta","lambda.sqe","xi.sqe","beta.sqe",
+                          "tbs.Max.AE","tbs.Mean.AE","orig.median","data.median",
+                          "tbs.median","tbs.sqe","run.time")
+
+  return(result.2)
+}
+
+
+
+aux <- sim.tbs(gen=list(n=100,cens=0,lambda=3,xi=2,beta=13,dist="logistic"),
+                n.copies=100,initial.seed=1235,method="Rsonlp",verbose=TRUE)
+
+print(apply(aux[,7:12],2,mean))
+print(apply(aux[,7:12],2,sd))
+
+if (FALSE) {
+# test das funcoes tbs
+dist   <- "logistic"
+lambda <- 3
+xi     <- 3
+beta   <- 2
+
+time <- rtbs(10000,lambda,xi,beta,NULL,dist)
+time <- sort(time)
+print(summary(time))
+emp <- ecdf(time)
+plot(emp)
+y <- ptbs(time,lambda,xi,beta,NULL,dist)
+lines(time,y,type="l",col=2)
+q <- qtbs(y,lambda,xi,beta,NULL,dist)
+lines(q,y,type="l",col=3,lty=2)
+
+print(summary(abs(emp(time)-y)))
+print(summary(abs(time-q)))
+
+hist(time,freq=FALSE)
+lines(time,dtbs(time,lambda,xi,beta,NULL,dist),col=2,type="l")
+
+
+}
