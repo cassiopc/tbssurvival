6c3caba71f327cca4b8d024d09dc099b95881116
polpo <polpo@avestruz.(none)>
2013-03-19 12:01:49 -0300

    fixed bugs in print.tbs.survreg.mle and summary.tbs.survreg.mle.

--- TBSSurvival/R/tbs.survreg.mle.r
+++ TBSSurvival/R/tbs.survreg.mle.r
@@ -1,5 +1,5 @@
 # TBSSurvival package for R (http://www.R-project.org)
-# Copyright (C) 2012 Adriano Polpo, Cassio de Campos, Debajyoti Sinha
+# Copyright (C) 2013 Adriano Polpo, Cassio de Campos, Debajyoti Sinha
 #                    Jianchang Lin and Stuart Lipsitz.
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -71,10 +71,10 @@ print.tbs.survreg.mle <- function(x, ...) {
   cat("TBS model with ",text.dist," error distribution.\n",sep="")
   cat("\n",sep="")
   cat("          point estimates (sd)\n",sep="")
-  cat("  lambda: ",sprintf("%.4f",x$par[1]),"  (",sprintf("%.4f",x$std.error[1]),")\n",sep="")
-  cat("      xi: ",sprintf("%.4f",x$par[2]),"  (",sprintf("%.4f",x$std.error[2]),")\n",sep="")
-  for (i in 3:length(x$par)) {
-  cat("   beta",i-3,": ",sprintf("%.4f",x$par[i]),"  (",sprintf("%.4f",x$std.error[i]),")\n",sep="")
+  cat("  lambda: ",sprintf("%.4f",x$lambda),"  (",sprintf("%.4f",x$lambda.std.error),")\n",sep="")
+  cat("      xi: ",sprintf("%.4f",x$xi),"  (",sprintf("%.4f",x$xi.std.error),")\n",sep="")
+  for (i in 1:length(x$beta)) {
+    cat("   beta",i-1,": ",sprintf("%.4f",x$beta[i]),"  (",sprintf("%.4f",x$beta.std.error[i]),")\n",sep="")
   }
   cat("\n",sep="")
   cat("    AIC: ",sprintf("%.4f",x$AIC),"\n",sep="")
@@ -101,18 +101,18 @@ summary.tbs.survreg.mle <- function(x, ...) {
   cat("TBS model with ",text.dist," error distribution.\n",sep="")
   cat("\n",sep="")
 
-  z.value <- rep(NA,length(x$par))
-  p.value <- rep(NA,length(x$par))
-  for (i in 3:length(x$par)) {
-    z.value[i] <- abs(x$par[i]/x$std.error[i])
+  z.value <- rep(NA,length(x$beta))
+  p.value <- rep(NA,length(x$beta))
+  for (i in 1:length(x$beta)) {
+    z.value[i] <- abs(x$beta[i]/x$beta.std.error[i])
     p.value[i] <- 2*pnorm(-z.value[i],0,1)
   }
 
-  aux1 <- nchar(trunc(x$par))+5
-  aux2 <- nchar(trunc(x$std.error))+5
-  aux3 <- nchar(trunc(z.value))+5
-  p.value2 <- rep(NA,length(x$par))
-  for (i in 3:length(x$par)) {
+  aux1 <- nchar(sprintf("%.4f",c(x$lambda,x$xi,x$beta)))
+  aux2 <- nchar(sprintf("%.4f",c(x$lambda.std.error,x$xi.std.error,x$beta.std.error)))
+  aux3 <- nchar(sprintf("%.4f",z.value))
+  p.value2 <- rep(NA,length(x$beta))
+  for (i in 1:length(x$beta)) {
     if (p.value[i] < 0.0001) {
       p.value2[i] <- "< 0.0001"
     } else {
@@ -134,19 +134,19 @@ summary.tbs.survreg.mle <- function(x, ...) {
                   rep(" ",auxc1[4]+1),text[4],"\n",sep="")
   auxc <- auxc+auxc1
   cat("  lambda: ",
-      rep(ifelse(auxc[1] > aux1[1]," ",""),abs(auxc[1]-aux1[1])),sprintf("%.4f",x$par[1])," ",
-      rep(ifelse(auxc[2] > aux2[1]," ",""),abs(auxc[2]-aux2[1])),sprintf("%.4f",x$std.error[1]),
+      rep(ifelse(auxc[1] > aux1[1]," ",""),abs(auxc[1]-aux1[1])),sprintf("%.4f",x$lambda)," ",
+      rep(ifelse(auxc[2] > aux2[1]," ",""),abs(auxc[2]-aux2[1])),sprintf("%.4f",x$lambda.std.error),
       "\n",sep="")
   cat("      xi: ",
-      rep(ifelse(auxc[1] > aux1[2]," ",""),abs(auxc[1]-aux1[2])),sprintf("%.4f",x$par[2])," ",
-      rep(ifelse(auxc[2] > aux2[2]," ",""),abs(auxc[2]-aux2[2])),sprintf("%.4f",x$std.error[2]),
+      rep(ifelse(auxc[1] > aux1[2]," ",""),abs(auxc[1]-aux1[2])),sprintf("%.4f",x$xi)," ",
+      rep(ifelse(auxc[2] > aux2[2]," ",""),abs(auxc[2]-aux2[2])),sprintf("%.4f",x$xi.std.error),
       "\n",sep="")
-  for (i in 3:length(x$par)) {
-    cat("   beta",i-3,": ",
-        rep(ifelse(auxc[1] > aux1[i]," ",""),abs(auxc[1]-aux1[i])),sprintf("%.4f",x$par[i])," ",
-        rep(ifelse(auxc[2] > aux2[i]," ",""),abs(auxc[2]-aux2[i])),sprintf("%.4f",x$std.error[i])," ",
-        rep(ifelse(auxc[3] > aux3[i]," ",""),abs(auxc[3]-aux3[i])),sprintf("%.4f",z.value[i])," ",
-        rep(ifelse(auxc[4] > aux4[i]," ",""),abs(auxc[4]-aux4[i])),p.value2[i],
+  for (i in 1:length(x$beta)) {
+    cat("   beta",i-1,": ",
+        rep(ifelse(auxc[1] > aux1[i+2]," ",""),abs(auxc[1]-aux1[i+2])),sprintf("%.4f",x$beta[i])," ",
+        rep(ifelse(auxc[2] > aux2[i+2]," ",""),abs(auxc[2]-aux2[i+2])),sprintf("%.4f",x$beta.std.error[i])," ",
+        rep(ifelse(auxc[3] > aux3[i+2]," ",""),abs(auxc[3]-aux3[i+2])),sprintf("%.4f",z.value[i])," ",
+        rep(ifelse(auxc[4] > aux4[i+2]," ",""),abs(auxc[4]-aux4[i+2])),p.value2[i],
         ifelse(p.value[i] < 0.0001," ***",ifelse(p.value[i] < 0.01," **",
                ifelse(p.value[i] < 0.05," *",ifelse(p.value[i] < 0.1," .","")))),
         "\n",sep="")
@@ -155,13 +155,13 @@ summary.tbs.survreg.mle <- function(x, ...) {
   cat("Summary statistic of the error for the TBS model\n",sep="")
   print(summary(x$error))
   cat("\n",sep="")
-  if (length(x$par) == 3) {
+  if (length(x$beta) == 1) {
     cat("Estimated quantiles of time event\n",sep="")
     aux1 <- qtbs(c(0.05,0.25,0.5,0.75,0.95),
-                 lambda=tbs.mle.norm$par[1],
-                 xi=tbs.mle.norm$par[2],
-                 beta=tbs.mle.norm$par[3],
-                 dist=tbs.mle.norm$error.dist)
+                 lambda=x$lambda,
+                 xi=x$xi,
+                 beta=x$beta,
+                 dist=x$error.dist)
     aux2 <- nchar(trunc(aux1))
     cat("   ",rep(" ",aux2[1]),"5%   ",rep(" ",aux2[2]),"25%   ",rep(" ",aux2[3]),
         "50%   ",rep(" ",aux2[4]),"75%   ",rep(" ",aux2[5]),"95%\n",sep="")
