79d81886d1e139fc78087ffb005d780da7e9f27e
cassiopc <cassiopc@gmail.com>
2013-04-07 15:34:26 +0200

    seed is global for mle

--- TBSSurvival/R/tbs.survreg.mle.r
+++ TBSSurvival/R/tbs.survreg.mle.r
@@ -29,13 +29,13 @@ tbs.survreg.mle <- function(formula,dist=dist.error("all"),method=c("Nelder-Mead
   ## for each of the methods given until one of them converges. It is supposed that at least one method
   ## is given, and that dist is one of those implemented by tbs.survreg.
   initial.time <- .gettime()
-  
+  set.seed(seed)
+
   fn.aux <- function(formula,dist,method,verbose,nstart,max.time) {
     initial.time <- .gettime()
     bestout <- NULL
     for(i in 1:length(method)) {
       ## call the estimation function
-      set.seed(seed)
       out <- .tbs.survreg(formula,dist=dist,method=method[i],verbose=verbose,max.time=max.time,nstart=nstart,gradient=gradient)
       ## if converged, we are happy
       if(out$convergence) {
